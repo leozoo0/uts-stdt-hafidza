cc8d640fad4109017735f13be13c496f6bda212a
leozooo <175855930+leozoo0@users.noreply.github.com>
2026-05-18 20:33:04 +0700

    Update app.py

--- loadbalancing-podman/app.py
+++ loadbalancing-podman/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import os
+import socket
 
 app = Flask(__name__)
 
+APP_NAME = os.getenv("APP_NAME", "UNKNOWN")
+
 @app.route("/")
 def home():
-    return f"Halo dari server: {os.getenv('SERVER_NAME')}"
+    hostname = socket.gethostname()
+    return f"Hello dari {APP_NAME} | Host: {hostname}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
